5f645f858050e610ba549da787862d3f5d826116
Michael wu <mwuf15@gmail.com>
2020-09-11 14:22:37 -0700

    working proxy of more place and calendar

--- server.js
+++ server.js
@@ -4,77 +4,77 @@ const port = 8888;
 const axios = require("axios");
 const parser = require("body-parser");
 
-app.listen(port, () => console.log(`Listening At Port ${port}`));
-app.use("/rooms/:roomId", express.static("public"));
-
-// reviews server
 app.use(parser.json());
 app.use(parser.urlencoded({ extended: true }));
+app.listen(port, () => console.log(`Listening At Port ${port}`));
+app.use('/properties/:id', express.static("public"));
 
-app.get("/reviews/users", (req, res) => {
-  axios
-    .get("http://13.52.214.87:3003/reviews/users")
-    .then((data) => {
-      res.status(200).send(data.data);
-    })
-    .catch(() => {
-      res.status(404);
-    });
-});
+// reviews server
 
-app.get("/reviews/houses", (req, res) => {
-  axios
-    .get("http://13.52.214.87:3003/reviews/houses")
-    .then((data) => {
-      res.status(200).send(data.data);
-    })
-    .catch(() => {
-      res.status(404);
-    });
-});
+// app.get("/properties/:id", (req, res) => {
+//   let id = req.params.id;
+//   axios
+//     .get(`http://184.169.237.98:3003/rooms/${id}/`)
+//     .then((data) => {
+//       res.status(200).send(data.data);
+//     })
+//     .catch(() => {
+//       res.status(404);
+//     });
+// });
 
-app.get("/reviews", (req, res) => {
-  axios
-    .get("http://13.52.214.87:3003/reviews")
-    .then((data) => {
-      res.status(200).send(data.data);
-    })
-    .catch(() => {
-      res.status(404);
-    });
-});
+// app.get("/reviews/houses", (req, res) => {
+//   axios
+//     .get("http://13.52.214.87:3003/reviews/houses")
+//     .then((data) => {
+//       res.status(200).send(data.data);
+//     })
+//     .catch(() => {
+//       res.status(404);
+//     });
+// });
 
-app.get("/reviews/comments", (req, res) => {
-  axios
-    .get("http://13.52.214.87:3003/reviews/comments")
-    .then((data) => {
-      res.status(200).send(data.data);
-    })
-    .catch(() => {
-      res.status(404);
-    });
-});
+// app.get("/reviews", (req, res) => {
+//   axios
+//     .get("http://13.52.214.87:3003/reviews")
+//     .then((data) => {
+//       res.status(200).send(data.data);
+//     })
+//     .catch(() => {
+//       res.status(404);
+//     });
+// });
+
+// app.get("/reviews/comments", (req, res) => {
+//   axios
+//     .get("http://13.52.214.87:3003/reviews/comments")
+//     .then((data) => {
+//       res.status(200).send(data.data);
+//     })
+//     .catch(() => {
+//       res.status(404);
+//     });
+// });
 
 // gallery server
 
-app.get("/properties/:id", (req, res) => {
-  let room_id = req.params.id;
-  axios
-    .get(`http://18.191.221.206:3001/properties/${room_id}`)
-    .then((response) => {
-      res.status(200).send(response.data);
-    })
-    .catch((err) => {
-      res.status(400).send(err);
-    });
-});
+// app.get("/properties/:id", (req, res) => {
+//   let room_id = req.params.id;
+//   axios
+//     .get(`http://18.191.221.206:3001/properties/${room_id}`)
+//     .then((response) => {
+//       res.status(200).send(response.data);
+//     })
+//     .catch((err) => {
+//       res.status(400).send(err);
+//     });
+// });
 
 // calendar server
 
-app.get("/rooms/:room_id/reservation", (req, res) => {
-  let queryParams = req.params.room_id;
-  axios
-    .get(`http://3.134.88.239:3002/rooms/${queryParams}/reservation`)
+app.get("/properties/:id/reservations", (req, res) => {
+  let queryParams = req.params.id;
+  axios.get(`http://52.53.165.191:6060/properties/${queryParams}/reservations`)
     .then((data) => {
       res.status(200).send(data.data);
     })
@@ -83,32 +83,33 @@ app.get("/rooms/:room_id/reservation", (req, res) => {
     });
 });
 
-app.post("/rooms/:room_id/reservation", (req, res) => {
-  let queryParams = req.params.room_id;
-  let reservation = {
-    check_in: req.body.check_in,
-    check_out: req.body.check_out,
-  };
+// app.post("/rooms/:room_id/reservation", (req, res) => {
+//   let queryParams = req.params.room_id;
+//   let reservation = {
+//     check_in: req.body.check_in,
+//     check_out: req.body.check_out,
+//   };
 
-  axios
-    .post(
-      `http://3.134.88.239:3002/rooms/${queryParams}/reservation`,
-      reservation
-    )
-    .then((data) => {
-      res.status(200).send(data.data);
-    })
-    .catch(() => {
-      res.status(404);
-    });
-});
+//   axios
+//     .post(
+//       `http://3.134.88.239:3002/rooms/${queryParams}/reservation`,
+//       reservation
+//     )
+//     .then((data) => {
+//       res.status(200).send(data.data);
+//     })
+//     .catch(() => {
+//       res.status(404);
+//     });
+// });
 
 // image carousel server
 
-app.get("/suggestedListings", (req, res) => {
-  axios
-    .get("http://52.32.67.233:3004/suggestedListings")
+app.get('/properties/:id/similiar', (req, res) => {
+  let id = req.params.id;
+  axios.get(`http://54.219.180.82:3004/properties/${id}/similiar`)
     .then((response) => {
+      // console.log('response',response.data)
       res.send(response.data);
     })
     .catch((err) => {
